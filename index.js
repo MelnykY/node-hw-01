2af97ab434a5afb057e9a9e550f8d879f5981a64
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-05-06 13:00:14 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Command } = require("commander");
+const program = new Command();
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.table(newContact);
+
+    case "remove":
+      const deleteById = await contacts.removeContact(id);
+      return console.table(deleteById);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
